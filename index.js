ec8f675d7c69a18b393f559bd95f08e93b64863a
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-06 21:53:05 +0530

    image/file uploading by Multer

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 import express from "express";
 import { loginController, SignupController } from "./controller.js";
 import router from "./Route.js";
+import multer from "multer";
+import storage from "./config/multer.js";
 
 const PORT = 3000;
-
 const app = express();
 app.use(express.json());
 
-//form using by urlencoded with an example of name and email
-app.use(express.urlencoded({ extended: true }));
+//Files uploading using multer by normal form-data in postman
+// storage is imported from ./config/multer.js file
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024000,
+  },
+});
+app.use(upload.single("image"));
 
 app.post("/form", (req, res) => {
-  console.log(req.body);
-  res.send("form recived");
+  console.log(req.file);
+  res.send("file form recived successfully");
 });
+
+//file upload end
 
 app.get("/", (req, res) => {
   res.send("Hello Beeresh");
