3a61d1ae1ccb5a48a96afd37e32bbd73c8022b2b
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-06 23:56:15 +0530

    CRUD method with creat new schema and store in DB

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import express from "express";
 import { loginController, SignupController } from "./controller.js";
 import router from "./Route.js";
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectDB from "./mongoDB.js";
+import { Person } from "./models/Person.js";
 
-dotenv.config();
 const PORT = 3000;
 const app = express();
 app.use(express.json());
 
 await connectDB();
+
+app.post("/person", async (req, res) => {
+  const { name, email, password } = req.body;
+  res.send("Person Added successfully");
+
+  const newPerson = new Person({
+    name,
+    email,
+    password,
+  });
+  await newPerson.save();
+  console.log(newPerson);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Beeresh");
