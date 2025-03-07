1a1f2adb6ddab16dc8411e00fa61466b92960ec6
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-07 16:19:02 +0530

    Delete items in DB by CRUD

--- index.js
+++ index.js
@@ -13,16 +13,20 @@ await connectDB();
 
 //Creating database
 app.post("/person", async (req, res) => {
-  const { name, email, password } = req.body;
-  res.send("Person Added successfully");
+  try {
+    const { name, email, password } = req.body;
 
-  const newPerson = new Person({
-    name,
-    email,
-    password,
-  });
-  await newPerson.save();
-  console.log(newPerson);
+    const newPerson = new Person({
+      name,
+      email,
+      password,
+    });
+    await newPerson.save();
+    console.log(newPerson);
+    res.send("Person Added successfully");
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 //Updating database
@@ -33,6 +37,12 @@ app.put("/person", async (req, res) => {
     name: "BBBBB",
   });
   res.send(personData);
+});
+
+app.delete("/person/:id", async (req, res) => {
+  const { id } = req.params;
+  await Person.findByIdAndDelete(id);
+  res.send("Person is deleted successfully");
 });
 
 app.get("/", (req, res) => {
