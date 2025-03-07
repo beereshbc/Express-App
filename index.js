06caeea474729fc59970d769ba6f4bfae63d5621
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-07 17:09:40 +0530

    WOrking with Cookies

--- index.js
+++ index.js
@@ -2,51 +2,27 @@ import express from "express";
 import { loginController, SignupController } from "./controller.js";
 import router from "./Route.js";
 import "dotenv/config";
-import connectDB from "./mongoDB.js";
-import { Person } from "./models/Person.js";
+import cookieParser from "cookie-parser";
 
 const PORT = 3000;
 const app = express();
-app.use(express.json());
 
-await connectDB();
-
-//Creating database
-app.post("/person", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const newPerson = new Person({
-      name,
-      email,
-      password,
-    });
-    await newPerson.save();
-    console.log(newPerson);
-    res.send("Person Added successfully");
-  } catch (error) {
-    res.send(error.message);
-  }
-});
-
-//Updating database
-app.put("/person", async (req, res) => {
-  const { id } = req.body;
-
-  const personData = await Person.findByIdAndUpdate(id, {
-    name: "BBBBB",
-  });
-  res.send(personData);
-});
-
-app.delete("/person/:id", async (req, res) => {
-  const { id } = req.params;
-  await Person.findByIdAndDelete(id);
-  res.send("Person is deleted successfully");
-});
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  res.cookie("name", "express-app", { maxAge: 36000 });
   res.send("Hello Beeresh");
+});
+
+//Manual method of removing cookies
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("name");
+  res.send("Cookie Cleared");
+});
+
+app.get("/fetch", (req, res) => {
+  console.log(req.cookies);
+  res.send("API Called");
 });
 
 app.listen(PORT, (req, res) => {
