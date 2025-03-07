6039df4935e8cbd386ccb9a2977628346e76eed2
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-07 18:05:36 +0530

    Session Management by using express-sessions

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 import express from "express";
-import { loginController, SignupController } from "./controller.js";
-import router from "./Route.js";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const PORT = 3000;
 const app = express();
 
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.cookie("name", "express-app", { maxAge: 36000 });
-  res.send("Hello Beeresh");
-});
-
-//Manual method of removing cookies
-app.get("/remove-cookie", (req, res) => {
-  res.clearCookie("name");
-  res.send("Cookie Cleared");
-});
+app.use(
+  session({
+    secret: "sample-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/fetch", (req, res) => {
-  console.log(req.cookies);
-  res.send("API Called");
+  if (req.session.page_viwes) {
+    req.session.page_viwes++;
+    res.send(`Your visited this site ${req.session.page_viwes} times`);
+  } else {
+    req.session.page_viwes = 1;
+    res.send("Your visited this site at first time Thank You");
+  }
+});
+
+app.get("/remove-session", (req, res) => {
+  req.session.destroy();
+  res.send("The session is deleted successufully");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Beeresh");
 });
 
 app.listen(PORT, (req, res) => {
