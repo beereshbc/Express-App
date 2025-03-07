b1fbf1f26b4844b7228053a128420d96bd120c20
bbcoder2005 <bcbeereshkumar@gmail.com>
2025-03-07 16:23:51 +0530

    alter the schema like Object type listing...

--- models/Person.js
+++ models/Person.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-const personSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const personSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    userOrder: { type: Object, default: {} },
+  },
+  { timestamps: true, minimize: false }
+); //minimize is added for save the userOrder as empty
 
 export const Person = mongoose.model("person", personSchema);
